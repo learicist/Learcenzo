0e86f7261f0e6864849dea6be91f884cb172d733
The Doctor <learicist@gmail.com>
2018-07-01 16:09:08 -0400

    testing

--- main.js
+++ main.js
@@ -4,26 +4,46 @@ const obj = {
 	'vaporeon': $('#not_pika')
 }
 
-$(document).ready(function () {
+let curr, str;
+
+$(document).ready(function () {	
 	
-	let ask = prompt("Please choose either Link, Master Chief, or Vaporeon."),
+	function yourMom() {
+		/*curr = */
+		let ask = prompt("Please choose either Link, Master Chief, or Vaporeon."), //response from user
 		str = '';
-	
-	for (let i in ask) {
-		str += ask[i].toLowerCase();
+		
+		for (let i in ask) {
+			str += ask[i].toLowerCase();//convert to lowercase
+		}
+		curr = obj[str];
+		if (curr == null) {  //if user is doo
+			alert("Ey, guy. That's not how we play, guy. Try again, budday.");
+			location.reload();  //try again
+		} else {  //otherwise
+			//console.log(str);
+			curr.removeClass('hidden');  //display user choice
+			$('#home').removeClass('hidden');  //display home btn
+		}
+		ask = '';
 	}
 	
-	if (obj[str] == null) {
-		alert("Ey, guy. That's not how we play, guy. Try again, budday.");
-		location.reload();
-	} else {
-		console.log(str);
-		obj[str].removeClass('hidden');
-		$('#home').removeClass('hidden');
-	}
+	yourMom();
 	
 	$('#home').click(function () {
-		location.reload();
+		
+		yourMom();
+		
+		for (let x in obj) {
+			
+			curr = obj[x]
+			if (!curr.hasClass('hidden')){
+				obj[x].addClass('hidden');
+				
+			}			
+			
+		}
+		
 	});
 	
 });
