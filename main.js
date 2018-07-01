7eb5db0aff86c97fe27353751265f960ec109cd4
The Doctor <learicist@gmail.com>
2018-07-01 14:46:56 -0400

    first commit on guest acct laptop vinny

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const obj = {
 	'link': $('#not_zelda'),
 	'master chief': $('#not_noble6'),
-	'vaporeon': $('#not_pika')
+	'vaporeon': $('#not_pika'),
+	'pr': $('#not_morty')
 }
 
 $(document).ready(function () {
 	
-	let ask = prompt("Please choose either Link, Master Chief, or Vaporeon."),
+	$('#not_morty').on('mouseenter', function (){
+		$(this).attr("title", "Click me for a video!");
+	});
+	
+	$('#pickleRick').on('click', function (){
+		$(this).addClass("hidden");
+		$('#pickleVid').removeClass('hidden');
+	});
+	
+	
+	let ask = prompt("Please choose either Link, Master Chief, pr, or Vaporeon."),
 		str = '';
 	
 	for (let i in ask) {
