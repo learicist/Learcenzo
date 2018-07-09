8705370f3e1c68e70849b3912c499a4dbc43b200
The Doctor <learicist@gmail.com>
2018-07-09 02:34:58 -0400

    Finalizing 7.8.18, introduction to object array and multiple images per option

--- main.js
+++ main.js
@@ -1,31 +1,73 @@
-const obj = {
-	'link': $('#not_zelda'),
-	'master chief': $('#not_noble6'),
-	'vaporeon': $('#not_pika'),
-	'pr': $('#not_morty')
-}
+const obj = [
+	{
+		name: 'zelda', 
+		tag: $('#zelda'), 
+		firstName: 'not_zelda', 
+		firstTag: $('#not_zelda'), 
+		secondName: 'real_zelda', 
+		secondTag: $('#real_zelda'), 
+		thirdName: 'ganon',
+		thirdTag: $('#ganon')
+	},
+	{
+		name: 'halo', 
+		tag: $('#halo'),
+		firstName: 'not_noble6',
+		firstTag: $('#not_noble6'),
+		secondName: 'captKeys',
+		secondTag: $('#captKeys'),
+		thirdName: 'elite',
+		thirdTag: $('#elite')
+	},
+	{
+		name: 'pokemon',
+		tag: $('#pokemon'),
+		firstName: 'not_pika',
+		firstTag: $('#not_pika'),
+		secondName: 'squirtle',
+		secondTag: $('#squirtle'),
+		thirdName: 'onyx',
+		thirdTag: $('#onyx')
+	},
+	{
+		name: 'mortick',
+		tag: $('#mortick'),
+		firstName: 'not_morty',
+		firstTag: $('#not_morty'),
+		secondName: 'morty',
+		secondTag: $('#morty'),
+		thirdName: 'summer',
+		thirdTag: $('#summer')
+	}
+]
 
 $(document).ready(function () {	
 
 	function yourMom() {
-		let ask = prompt("Please choose either Link, Master Chief, PR, or Vaporeon.");
-		let curr, str = ask.toLowerCase();
-		
-		console.log(str);
-		curr = obj[str];
-		
-		if (curr == null) {  //if user is doo
+		let ask = prompt("Please choose either Zelda, Halo, Mortick, or Pokemon.");
+		let curr;
+		if (ask === null || ask == '') {  //if user is doo
 			alert("Ey, guy. That's not how we play, guy. Try again, budday.");
 			location.reload(); 
 		} else { 
+			let str = ask.toLowerCase();
+			console.log(str);
+			for (let i in obj) {
+				if (obj[i].name == str) {
+					curr = obj[i].tag;
+					//console.log(curr);
+				}
+			}
 			curr.removeClass('hidden');  //display user choice
-			$('#home').removeClass('hidden');  //display home btn
+			$('#home').removeClass('hidden');  //display home btn;
 		} 
+		
+		
 	}
 	
 	$('#begin').click(function () {
-		$('#startBtn #begin').addClass('hidden');
 		yourMom();
+		$('.start').addClass('hidden');
 	});
 	
 	
@@ -35,10 +77,6 @@ $(document).ready(function () {
 	});
 	
 	$('#home').click(function () {
-		$('.col-sm-6').each(function() {
-			$(this).addClass('hidden');
-			//console.log($(this).hasClass('hidden'));
-		});
-		yourMom();
+		location.reload();
 	});
 });
